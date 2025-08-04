7f9e08db84db070c2f8162889f900e1a84f58b0d
Natanael Martins <natanael1337martins@gmail.com>
2025-08-04 20:05:55 -0300

    DeclaracaoVariavelDecimalFormatado

---
+++ 02_Variaveis/DeclaracaoVariavelDecimalFormatado.cs
@@ -0,0 +1,12 @@
+using System;
+class DeclaracaoVariaveisDecimalFormatado
+{
+	static void main(string[] args)
+	{
+		decimal x = 0.999m;
+		decimal Y = 9999999999999999999999999999
+		console.Writeline("Minha quantia = {0:c}", x);
+		console.Writeline("sua quantia = {0:c}", y);
+
+	}
+}
