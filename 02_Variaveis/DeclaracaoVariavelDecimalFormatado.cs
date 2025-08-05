38e68832f7ca0a5fc04536394304d60e312b24e2
Natanael Martins <natanael1337martins@gmail.com>
2025-08-04 21:01:53 -0300

    Criação do codigo C#

--- 02_Variaveis/DeclaracaoVariavelDecimalFormatado.cs
+++ 02_Variaveis/DeclaracaoVariavelDecimalFormatado.cs
@@ -1,12 +1,18 @@
 using System;
 class DeclaracaoVariaveisDecimalFormatado
 {
-	static void main(string[] args)
+	static void Main(string[] args)
 	{
 		decimal x = 0.999m;
-		decimal Y = 9999999999999999999999999999
-		console.Writeline("Minha quantia = {0:c}", x);
-		console.Writeline("sua quantia = {0:c}", y);
-
+		decimal y = 9999999999999999999999999999m;
+		/* Utilizado*/
+		Console.WriteLine("Minha quantia = {0:c}", x);
+		Console.WriteLine("Sua quantia = {0:c}", y);
+		Console.WriteLine("Valor de x = {0:F3} e Valor de y = {1:c}",x,y);
+		/*
+		Console.WriteLine($"Minha quantia = {x:c}", x);
+		Console.WriteLine($"Sua quantia = {y:c}", y);
+		Console.WriteLine($"Valor de x = {x:F3} e Valor de y = {y:c}",x,y);
+		*/
 	}
 }
