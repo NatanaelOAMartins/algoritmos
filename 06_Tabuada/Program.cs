d711cd21e3d5ef8d9cba34f106928f2ac71eb73d
Natanael Martins <natanael1337martins@gmail.com>
2025-08-08 22:20:38 -0300

    tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite um numero:");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nTabuada do {numero}:");
+            for (int i = 1; i <= 10; i++)
+            { 
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+        }
+    }
+}
